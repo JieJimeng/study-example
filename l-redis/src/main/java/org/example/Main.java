6ac831d57f8570cc578b943f89431e250fa3bda8
jiejimeng <jiejimeng@agree.com.cn>
2024-12-30 13:35:11 +0800

    bb

--- l-redis/src/main/java/org/example/Main.java
+++ l-redis/src/main/java/org/example/Main.java
@@ -8,6 +8,6 @@ package org.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Worlds!");
+        System.out.println("Hello, Worldssssssssss!");
     }
 }
