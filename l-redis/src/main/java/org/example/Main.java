92df86940fb2fecbc94e565063a63a905c6212e5
“jiejimeng” <jiejimeng@agree.com.cn>
2024-12-30 12:15:52 +0800

    Init l-redis

---
+++ l-redis/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * ${TODO}
+ *
+ * @author jieji
+ * @date 2024/12/30
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
